322c5d19725408171c1e9678395b3e33f4129df4
lightnonstop <umichaeledet003@gmail.com>
2023-10-11 13:57:23 +0100

    add product catalogue

---
+++ client/src/features/catalog/Catalog.tsx
@@ -0,0 +1,25 @@
+import { Avatar, Button, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import { Product } from "../../app/models/product"
+
+interface CatalogProps {
+    products: Product[];
+    addProduct: () => void;
+}
+function Catalog({ products, addProduct }: CatalogProps) {
+    return (
+        <>
+            <List>
+                {products.map(product => (
+                    <ListItem key={product.id}>
+                        <ListItemAvatar>
+                            <Avatar src={product.pictureUrl} />
+                        </ListItemAvatar>
+                        <ListItemText>{product.name} - {product.price}</ListItemText>
+                    </ListItem>
+                ))}
+            </List>
+            <Button variant="contained" onClick={addProduct}>Add Product</Button>
+        </>
+    )
+}
+export default Catalog
