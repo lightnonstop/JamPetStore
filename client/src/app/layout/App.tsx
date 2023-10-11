d736e0780868ecb478c68bf72f09abf128d3712d
lightnonstop <umichaeledet003@gmail.com>
2023-10-11 14:59:54 +0100

    move code to catalog component

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,33 +1,16 @@
-import { useEffect, useState } from "react"
 import Catalog from "../../features/catalog/Catalog"
-import { Product } from "../models/product"
-import { CssBaseline } from "@mui/material";
+import { Container, CssBaseline } from "@mui/material";
 import Header from "./Header";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    fetch("http://localhost:5000/api/products")
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, [])
-
-  function addProduct() {
-    setProducts(prevState => [...prevState, {
-      id: prevState.length + 101,
-      name: 'product' + prevState.length + 1,
-      price: prevState.length * 100 + 100,
-      brand: 'some brand',
-      description: 'some description',
-      pictureUrl: 'http://picsum.photos/200'
-    }])
-  }
 
   return (
     <>
+      <CssBaseline />
       <Header />
-      <Catalog products={products} addProduct={addProduct} />
+      <Container>
+        <Catalog />
+      </Container>
     </>
   )
 }
