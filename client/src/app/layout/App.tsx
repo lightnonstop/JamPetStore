d5e7c432a18e6cf4318d893c59fdca3fba258fde
lightnonstop <umichaeledet003@gmail.com>
2023-10-11 13:57:40 +0100

    add page heading

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react"
+import Catalog from "../../features/catalog/Catalog"
+import { Product } from "../models/product"
+import { Typography } from "@mui/material";
+
 function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/products")
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
+
+  function addProduct() {
+    setProducts(prevState => [...prevState, {
+      id: prevState.length + 101,
+      name: 'product' + prevState.length + 1,
+      price: prevState.length * 100 + 100,
+      brand: 'some brand',
+      description: 'some description',
+      pictureUrl: 'http://picsum.photos/200'
+    }])
+  }
 
   return (
-    <div>
-      <h1>JamStore</h1>
-    </div>
+    <>
+      <Typography variant="h1">Ski Kit Mart</Typography>
+      <Catalog products={products} addProduct={addProduct} />
+    </>
   )
 }
 
