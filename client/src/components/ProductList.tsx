a6c3239ccc102e6887d2d06e3482503b927d4689
lightnonstop <umichaeledet003@gmail.com>
2023-10-12 03:58:09 +0100

    list products in grid

---
+++ client/src/components/ProductList.tsx
@@ -0,0 +1,20 @@
+import { Grid } from "@mui/material"
+import ProductCard from "./ProductCard";
+import { Product } from "../app/models/product";
+
+interface ProductListProps {
+    products: Product[];
+}
+
+function ProductList({ products }: ProductListProps) {
+    return (
+        <Grid container spacing={4}>
+            {products.map(product => (
+                <Grid item xs={3} key={product.id}>
+                    <ProductCard product={product} />
+                </Grid>
+            ))}
+        </Grid>
+    )
+}
+export default ProductList
