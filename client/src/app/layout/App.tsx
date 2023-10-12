f99fe0dbe9b98fbe5691d99097f47b3f322eec07
lightnonstop <umichaeledet003@gmail.com>
2023-10-12 03:57:48 +0100

    set screen mode

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,17 +1,28 @@
-import Catalog from "../../features/catalog/Catalog"
-import { Container, CssBaseline } from "@mui/material";
+import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import Header from "./Header";
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === "light" ? '#EAEAEA' : '#121212'
+      }
+    },
+  });
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
+      <Header setDarkMode={setDarkMode} />
       <Container>
-        <Catalog />
+        <Outlet />
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
