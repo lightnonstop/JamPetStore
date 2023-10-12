bdb5c637425f0c2c6f87be43a94396887d87aad9
lightnonstop <umichaeledet003@gmail.com>
2023-10-12 03:56:31 +0100

    add nav header

---
+++ client/src/app/layout/Header.tsx
@@ -0,0 +1,74 @@
+import { ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { Dispatch, SetStateAction } from "react";
+import { NavLink } from "react-router-dom";
+
+const links = {
+    middle: [
+        { title: 'catalog', path: '/catalog' },
+        { title: 'about', path: '/about' },
+        { title: 'contact', path: '/contact' },
+    ],
+    right: [
+        { title: 'login', path: '/login' },
+        { title: 'register', path: '/register' },
+    ]
+}
+
+const navLinkStyle = {
+    color: "inherit", typography: "h6", "&:hover": {
+        color: "secondary.main"
+    },
+    textDecoration: "none",
+    '&.active': {
+        color: "text.secondary"
+    }
+}
+
+interface HeaderProps {
+    setDarkMode: Dispatch<SetStateAction<boolean>>;
+}
+function Header({ setDarkMode }: HeaderProps) {
+    return (
+        <AppBar position="static" sx={{ mb: 4 }}>
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Box display="flex" alignItems="center">
+                    <Typography variant="h6" component={NavLink} to="/" sx={{ color: "inherit", textDecoration: "none", mr: 1 }}>Ski Store</Typography>
+                    <Switch onChange={() => setDarkMode(prev => !prev)} size="small" />
+                </Box>
+                <List sx={{ display: "flex" }}>
+                    {links.middle.map(({ title, path }) => (
+                        <ListItem
+                            component={NavLink}
+                            to={path}
+                            key={path}
+                            sx={navLinkStyle}
+                        >
+                            {title.toUpperCase()}
+                        </ListItem>
+                    ))}
+                </List>
+                <Box display="flex" alignItems="center">
+                    <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}>
+                        <Badge badgeContent='4' color='secondary'>
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
+                    <List sx={{ display: "flex" }}>
+                        {links.right.map(({ title, path }) => (
+                            <ListItem
+                                component={NavLink}
+                                to={path}
+                                key={path}
+                                sx={navLinkStyle}
+                            >
+                                {title.toUpperCase()}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    )
+}
+export default Header
