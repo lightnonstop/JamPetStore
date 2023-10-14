77642afb50587baf9855d3e80bbf3fcf5a351e8a
lightnonstop <umichaeledet003@gmail.com>
2023-10-14 23:24:13 +0100

    fix: error in url string

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -16,7 +16,7 @@ namespace API.Controllers
             return BadRequest(new ProblemDetails { Title = "This is a bad request" });
         }
 
-        [HttpGet("unathorized")]
+        [HttpGet("unauthorized")]
         public ActionResult GetUnauthorized()
         {
             return Unauthorized();
