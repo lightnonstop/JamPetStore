0d0b5d55d83eaaf67e58b0497f2dd8b49a5792e7
lightnonstop <umichaeledet003@gmail.com>
2023-10-15 15:38:22 +0100

    add body for editing requests

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -42,8 +42,8 @@ axios.interceptors.response.use(
 );
 const requests = {
   get: (url: string) => axios.get(url).then(responseBody),
-  post: (url: string) => axios.post(url).then(responseBody),
-  put: (url: string) => axios.put(url).then(responseBody),
+  post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
+  put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
   delete: (url: string) => axios.delete(url).then(responseBody),
 };
 
