c31b1879f8e971036fe8af2ac4ff2cfea627d87f
lightnonstop <umichaeledet003@gmail.com>
2023-10-15 15:12:06 +0100

    use loading component, type loading as boolean

--- client/src/app/pages/catalog/Catalog.tsx
+++ client/src/app/pages/catalog/Catalog.tsx
@@ -2,13 +2,19 @@ import ProductList from "../../../components/ProductList";
 import { useEffect, useState } from "react";
 import { Product } from "../../models/product";
 import { agent } from "../../api/agent";
+import LoadingComponent from "../../layout/LoadingComponent";
 
 function Catalog() {
     const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
         agent.Catalog.list().then(products => setProducts(products))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false))
     }, [])
+
+    if (loading) return <LoadingComponent message="Loading products..." />
 
     return (
         <>
