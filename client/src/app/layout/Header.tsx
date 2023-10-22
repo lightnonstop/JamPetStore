33030a98ca813ba15756c460687c6a3a25847b89
lightnonstop <umichaeledet003@gmail.com>
2023-10-22 15:09:56 +0100

    add useful imports

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,7 +1,8 @@
 import { ShoppingCart } from "@mui/icons-material";
 import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useMemo } from "react";
 import { Link, NavLink } from "react-router-dom";
+import { useStoreContext } from "../context/StoreContext";
 
 const links = {
     middle: [
