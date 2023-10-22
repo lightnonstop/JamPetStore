1f1132c786438909402ca472064d2c3d3949336f
lightnonstop <umichaeledet003@gmail.com>
2023-10-22 02:29:03 +0100

    import links

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,7 +1,7 @@
 import { ShoppingCart } from "@mui/icons-material";
 import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
 import { Dispatch, SetStateAction } from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const links = {
     middle: [
