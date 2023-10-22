1e385553cc5d7b3caa03faf6f1035a762a16746d
lightnonstop <umichaeledet003@gmail.com>
2023-10-22 03:28:19 +0100

    define app store state context

---
+++ client/src/app/context/StoreContext.tsx
@@ -0,0 +1,40 @@
+import { PropsWithChildren, createContext, useContext, useState } from "react";
+import { BasketProps } from "../models/basket";
+
+interface StoreContextValue {
+    basket: BasketProps | null;
+    setBasket: (basket: BasketProps) => void;
+    removeItem: (productId: number, quantity: number) => void;
+}
+
+export const StoreContext = createContext<StoreContextValue | undefined>(undefined);
+
+// eslint-disable-next-line react-refresh/only-export-components
+export function useStoreContext() {
+    const context = useContext(StoreContext);
+
+    if (!context === undefined) {
+        throw Error('Oops - we do not seem to be inside the context provider');
+    }
+
+    return context;
+}
+
+function StoreProvider({ children }: PropsWithChildren) {
+    const [basket, setBasket] = useState<BasketProps | null>(null);
+
+    function removeItem(productId: number, quantity: number) {
+        if (!basket) return;
+        const items = [...basket.items];
+        const itemIndex = items.findIndex(i => i.productId === productId);
+        if (itemIndex >= 0) {
+            items[itemIndex].quantity -= quantity;
+            if (items[itemIndex].quantity === 0) items.splice(itemIndex, 1);
+            setBasket(prevState => ({ ...prevState!, items }))
+        }
+    }
+    return (
+        <StoreContext.Provider value={{ basket, setBasket, removeItem }}>{children}</StoreContext.Provider>
+    )
+}
+export default StoreProvider
