98a2508637c6cba43b9d2b08ca8440eb5172dcf5
lightnonstop <umichaeledet003@gmail.com>
2023-10-26 00:33:03 +0100

    code refactor to use redux store

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -7,9 +7,11 @@ import 'react-toastify/dist/ReactToastify.css';
 import { getCookie } from "../util/util";
 import { agent } from "../api/agent";
 import LoadingComponent from "./LoadingComponent";
+import { useAppDispatch } from "../store/configureStore";
+import { setBasket } from "../pages/basket/basketSlice";
 
 function App() {
-  const { setBasket } = useStoreContext();
+  const dispatch = useAppDispatch();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -17,11 +19,11 @@ function App() {
     if (buyerId) {
 
       agent.Basket.get()
-        .then(basket => setBasket(basket))
+        .then(basket => dispatch(setBasket(basket)))
         .catch(error => console.log(error))
         .finally(() => setLoading(false))
     }
-  }, [setBasket])
+  }, [dispatch])
 
   const [darkMode, setDarkMode] = useState<boolean>(false);
   const paletteType = darkMode ? 'dark' : 'light';
