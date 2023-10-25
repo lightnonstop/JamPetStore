925b3241bbf2452ca9ff032d90e47242070cc296
lightnonstop <umichaeledet003@gmail.com>
2023-10-26 00:31:01 +0100

    remove react context store import

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import { useStoreContext } from "../context/StoreContext";
 import { getCookie } from "../util/util";
 import { agent } from "../api/agent";
 import LoadingComponent from "./LoadingComponent";
