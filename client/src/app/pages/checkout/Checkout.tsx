caa035ec3334add2f840a2118f5c842e7c5b5c4e
lightnonstop <umichaeledet003@gmail.com>
2023-10-25 03:06:27 +0100

    add checkout page

---
+++ client/src/app/pages/checkout/Checkout.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+
+function Checkout() {
+    return (
+        <Typography variant="h3">
+            Only logged in users can see this
+        </Typography>
+    )
+}
+export default Checkout
