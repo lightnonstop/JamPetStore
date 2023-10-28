c7c8e570e1dc7663261e407d4795c5cb0f4b1f58
lightnonstop <umichaeledet003@gmail.com>
2023-10-28 06:43:00 +0100

    add catalog slice

---
+++ client/src/app/pages/catalog/catalogSlice.ts
@@ -0,0 +1,43 @@
+import {
+  createAsyncThunk,
+  createEntityAdapter,
+  createSlice,
+} from "@reduxjs/toolkit";
+import { Product } from "../../models/product";
+import { agent } from "../../api/agent";
+
+const productsAdapter = createEntityAdapter<Product>();
+
+console.log(productsAdapter);
+
+export const fetchProductsAsync = createAsyncThunk<Product[]>(
+  "catalog/fetchProductAsync",
+  async () => {
+    try {
+      return await agent.Catalog.list();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+export const catalogSlice = createSlice({
+  name: "catalog",
+  initialState: productsAdapter.getInitialState({
+    productLoaded: false,
+    status: "idle",
+  }),
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchProductsAsync.pending, (state) => {
+      state.status = "pendingFetchProducts";
+    });
+    builder.addCase(fetchProductsAsync.fulfilled, (state, action) => {
+      productsAdapter.setAll(state, action.payload);
+      state.status = "idle";
+      state.productLoaded = true;
+    });
+    builder.addCase(fetchProductsAsync.rejected, (state) => {
+      state.status = "idle";
+    });
+  },
+});
