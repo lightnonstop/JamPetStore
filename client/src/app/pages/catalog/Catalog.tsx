cd2e7316878ea27f611f4bc5da1da81dc0227033
lightnonstop <umichaeledet003@gmail.com>
2023-10-30 03:33:01 +0100

    fetch filters from catalog page

--- client/src/app/pages/catalog/Catalog.tsx
+++ client/src/app/pages/catalog/Catalog.tsx
@@ -2,17 +2,21 @@ import ProductList from "../../../components/ProductList";
 import { useEffect } from "react";
 import LoadingComponent from "../../layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "../../store/configureStore";
-import { fetchProductsAsync, productSelectors } from "./catalogSlice";
+import { fetchFilters, fetchProductsAsync, productSelectors } from "./catalogSlice";
 
 function Catalog() {
     const products = useAppSelector(productSelectors.selectAll)
-    const { productsLoaded, status } = useAppSelector(state => state.catalog)
+    const { productsLoaded, status, filtersLoaded } = useAppSelector(state => state.catalog)
 
     const dispatch = useAppDispatch();
 
     useEffect(() => {
         if (!productsLoaded) dispatch(fetchProductsAsync());
     }, [dispatch, productsLoaded])
+
+    useEffect(() => {
+        if (!filtersLoaded) dispatch(fetchFilters());
+    }, [dispatch, filtersLoaded])
 
     if (status.includes('pending')) return <LoadingComponent message="Loading products..." />
 
