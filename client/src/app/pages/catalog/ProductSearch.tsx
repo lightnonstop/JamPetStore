ee2bca9008cab04c03ab45f63a58f8a777d5433c
lightnonstop <umichaeledet003@gmail.com>
2023-10-31 16:22:01 +0100

    add product search component

---
+++ client/src/app/pages/catalog/ProductSearch.tsx
@@ -0,0 +1,8 @@
+import { TextField } from "@mui/material";
+
+function ProductSearch() {
+    return (
+        <TextField label='Search' variant='outlined' fullWidth />
+    )
+}
+export default ProductSearch
