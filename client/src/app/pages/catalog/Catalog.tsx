33f365f599c477ab3d87a960c05cc0dc73321d30
lightnonstop <umichaeledet003@gmail.com>
2023-10-31 14:56:27 +0100

    edit catalog template

--- client/src/app/pages/catalog/Catalog.tsx
+++ client/src/app/pages/catalog/Catalog.tsx
@@ -28,9 +28,48 @@ function Catalog() {
     if (status.includes('pending')) return <LoadingComponent message="Loading products..." />
 
     return (
-        <>
-            <ProductList products={products} />
-        </>
+        <Grid container spacing={4}>
+            <Grid item xs={3}>
+                <Paper sx={{ mb: 2 }}>
+                    <TextField label="Search" variant="outlined" fullWidth />
+                </Paper>
+                <Paper sx={{ mb: 2, p: 2 }}>
+                    <FormControl component="fieldset">
+                        <RadioGroup>
+                            {sortOptions.map(({ value, label }, index) => (
+                                <FormControlLabel key={index} value={value} control={<Radio />} label={label} />
+                            ))}
+                        </RadioGroup>
+                    </FormControl>
+                </Paper>
+                <Paper sx={{ mb: 2, p: 2 }}>
+                    <FormGroup>
+                        {brands.map(brand => (
+                            <FormControlLabel control={<Checkbox />} label={brand} key={brand} />
+                        ))}
+                    </FormGroup>
+                </Paper>
+                <Paper sx={{ mb: 2, p: 2 }}>
+                    <FormGroup>
+                        {types.map(type => (
+                            <FormControlLabel control={<Checkbox />} label={type} key={type} />
+                        ))}
+                    </FormGroup>
+                </Paper>
+            </Grid>
+            <Grid item xs={9}>
+                <ProductList products={products} />
+            </Grid>
+            <Grid item xs={3} />
+            <Grid item xs={9}>
+                <Box display="flex" justifyContent="space-between" alignItems="center">
+                    <Typography>Displaying 1-6 of 20 items</Typography>
+                    <Pagination color="secondary" size="large" count={10} page={2} />
+                </Box>
+            </Grid>
+
+
+        </Grid>
     )
 }
 export default Catalog
