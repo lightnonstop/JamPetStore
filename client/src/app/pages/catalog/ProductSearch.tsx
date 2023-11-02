0bb456b604e2530b45d749c71f0c4c329db75e52
lightnonstop <umichaeledet003@gmail.com>
2023-11-02 17:01:08 +0100

    make search box functional

--- client/src/app/pages/catalog/ProductSearch.tsx
+++ client/src/app/pages/catalog/ProductSearch.tsx
@@ -1,8 +1,27 @@
-import { TextField } from "@mui/material";
+import { TextField, debounce } from "@mui/material";
+import { useAppDispatch, useAppSelector } from "../../store/configureStore";
+import { setProductParams } from "./catalogSlice";
+import { useState } from "react";
 
 function ProductSearch() {
+    const { productParams } = useAppSelector(state => state.catalog);
+    const dispatch = useAppDispatch();
+    const [searchTerm, setSearchTerm] = useState(productParams.searchTerm);
+
+    const debouncedSearch = debounce((event: any) => {
+        dispatch(setProductParams({ searchTerm: event.target.value }))
+    }, 1000)
+
     return (
-        <TextField label='Search' variant='outlined' fullWidth />
+        <TextField
+            label='Search'
+            variant='outlined'
+            fullWidth
+            value={searchTerm || ''}
+            onChange={event => {
+                setSearchTerm(event.target.value);
+                debouncedSearch(event);
+            }} />
     )
 }
 export default ProductSearch
