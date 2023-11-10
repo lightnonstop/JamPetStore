c2b4e264980b9ac176564e0484c23885da4a858a
lightnonstop <umichaeledet003@gmail.com>
2023-11-10 08:21:11 +0100

    use skeleton display if product is unavailable

--- client/src/components/ProductList.tsx
+++ client/src/components/ProductList.tsx
@@ -1,17 +1,24 @@
 import { Grid } from "@mui/material"
 import ProductCard from "./ProductCard";
 import { Product } from "../app/models/product";
+import { useAppSelector } from "../app/store/configureStore";
+import ProductCardSkeleton from "../app/pages/catalog/ProductCardSkeleton";
 
 interface ProductListProps {
     products: Product[];
 }
 
 function ProductList({ products }: ProductListProps) {
+    const { productsLoaded } = useAppSelector(state => state.catalog);
     return (
         <Grid container spacing={4}>
             {products.map(product => (
                 <Grid item xs={4} key={product.id}>
-                    <ProductCard product={product} />
+                    {!productsLoaded ? (
+                        <ProductCardSkeleton />
+                    ) : (
+                        <ProductCard product={product} />
+                    )}
                 </Grid>
             ))}
         </Grid>
