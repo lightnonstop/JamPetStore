3df2303734b5656d34b03c1d11d0163adf6a4557
lightnonstop <umichaeledet003@gmail.com>
2023-11-27 09:40:02 +0100

    edit login page with react hook form

--- client/src/app/pages/account/Login.tsx
+++ client/src/app/pages/account/Login.tsx
@@ -1,23 +1,18 @@
-
-import { LockOutlined } from "@mui/icons-material";
-import { Avatar, Box, Button, Container, Grid, Paper, TextField, Typography } from "@mui/material";
-import { useState } from "react";
+import { LockOutlined } from "@mui/icons-material"
+import { Avatar, Box, Container, Grid, Paper, TextField, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import { agent } from "../../api/agent";
+import { FieldValues, useForm } from "react-hook-form";
+import { LoadingButton } from "@mui/lab";
 
 function Login() {
-    const [values, setValues] = useState({
-        username: '',
-        password: ''
-    })
-    const handleSubmit = (event: any) => {
-        event.preventDefault();
-        agent.Account.login(values);
+
+    const { register, handleSubmit, formState: { isSubmitting } } = useForm();
+
+    async function submitForm(data: FieldValues) {
+        await agent.Account.login(data);
     }
-    const handleInputChange = function (event: any) {
-        const { name, value } = event.target;
-        setValues({ ...values, [name]: value })
-    }
+
     return (
         <Container component={Paper} maxWidth="sm" sx={{ display: "flex", flexDirection: "column", alignItems: "center", p: 4 }}>
             <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
@@ -26,12 +21,12 @@ function Login() {
             <Typography>
                 Sign in
             </Typography>
-            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                <TextField margin="normal" fullWidth label="Username" name="username" autoFocus onChange={handleInputChange} value={values.username} />
-                <TextField margin="normal" fullWidth id="password" label="Password" name="password" type="password" autoFocus onChange={handleInputChange} value={values.password} />
-                <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+            <Box component="form" onSubmit={handleSubmit(submitForm)} noValidate sx={{ mt: 1 }}>
+                <TextField margin="normal" fullWidth label="Username" autoFocus {...register('username')} />
+                <TextField margin="normal" fullWidth id="password" label="Password" type="password" autoFocus {...register('password')} />
+                <LoadingButton loading={isSubmitting} type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
                     Sign In
-                </Button>
+                </LoadingButton>
                 <Grid container>
                     <Grid item>
                         <Link to="/register">
