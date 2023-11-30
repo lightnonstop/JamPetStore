e302e47a9404d7eaad106e16acc393073ce2dca3
lightnonstop <umichaeledet003@gmail.com>
2023-11-30 09:44:47 +0100

    add account reducer

--- client/src/app/store/configureStore.ts
+++ client/src/app/store/configureStore.ts
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { basketSlice } from "../pages/basket/basketSlice";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { catalogSlice } from "../pages/catalog/catalogSlice";
+import { accountSlice } from "../pages/account/accountSlice";
 
 export const store = configureStore({
   reducer: {
     basket: basketSlice.reducer,
     catalog: catalogSlice.reducer,
+    account: accountSlice.reducer,
   },
 });
 
